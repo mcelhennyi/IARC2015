5ba89961004d281512e2d016c3e6c83b67b5d4b2
mcelhennyi <mcelhenny_ian@yahoo.com>
2015-09-12 19:09:15 +0000

    Basic structure
    Basic structure

---
+++ Main/example_code.py
@@ -0,0 +1,9 @@
+__author__ = 'odroid'
+
+class ExampleCode():
+    pass
+    # This is how a directory should be set up. The folder name is CamelCase and under scores between the words
+    # The file of a code should have a name that is all lower case with underscore between words
+    # The class name should be camel-case with no underscore or space.
+    # There should be no spaces in any folder/file/class/variable/ect.
+    # Follow PEP-8 guidelines
