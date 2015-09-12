62b6d7faaccaec12f0f2c33638f5d7bb92c2cfd6
mcelhennyi:F22raptor <mcelhenny_ian@yahoo.com>
2015-09-12 20:01:56 +0000

    Added more folders

--- Main/example_code.py
+++ Main/example_code.py
@@ -1,8 +1,8 @@
-__author__ = 'odroid'
+__author__ = 'odroid'  # Your name here
 
 class ExampleCode():
     pass
-    # This is how a directory should be set up. The folder name is CamelCase and under scores between the words
+    # This is how a directory should be set up. The folder name is CamelCase with under scores between the words
     # The file of a code should have a name that is all lower case with underscore between words
     # The class name should be camel-case with no underscore or space.
     # There should be no spaces in any folder/file/class/variable/ect.
